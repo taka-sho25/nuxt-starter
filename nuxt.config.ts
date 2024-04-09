b496545edddf355e54721cd62741f40dc369940c
shogo-takao <taka.sho2508@gmail.com>
2024-04-09 23:16:13 +0900

    add setting, base component

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/storybook'],
+
+  components: [
+    {
+      path: '~/components',
+      pathPrefix: false,
+      extensions: ['vue'],
+    },
+  ],
+});
